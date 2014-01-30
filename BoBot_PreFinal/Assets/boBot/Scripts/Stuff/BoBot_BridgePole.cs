936f6f8703beca1039e55cbaffe5b2bdbb5cef01
ma2chek <info@felixmatuschek.de>
2014-01-30 17:06:17 +0100

    swsw

--- BoBot_PreFinal/Assets/boBot/Scripts/Stuff/BoBot_BridgePole.cs
+++ BoBot_PreFinal/Assets/boBot/Scripts/Stuff/BoBot_BridgePole.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class BoBot_BridgePole : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		this.rigidbody.MovePosition(this.rigidbody.transform.position + Vector3.right * 2.2f);
 	}
 }
